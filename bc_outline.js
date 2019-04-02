06bf95377d5bde6c705cd868f1c7ec4755ad289e
Melinda Chirila <mchirila980@west-mec.org>
2019-04-02 13:53:14 -0700

    Final commit
    project ready for review.

--- bc_outline.js
+++ bc_outline.js
@@ -43,34 +43,47 @@ function makeOutline() {
 }
 
 function createList(source, outlineList) {
-
+      //heading for the outline
       var headings = ["H1", "H2", "H3", "H4", "H5", "H6"];
+      //previous level of the headings
       var prevLevel = 0;
-
+      //running total of the article headings
+      var headNum = 0;
+      //loop through all of the child nodes of source article until no child nodes are left behind
       for (var n = source.firstChild; n !== null; n = n.nextSibling) {
-      var headLevel = headings.indexOf(n.nodeName);
-      if (headLevel !== -1) {
-      var listElem = document.createElement("li");
-      listElem.innerHTML = n.firstChild.nodeValue;
-      if (headLevel === prevLevel) {
-      outlineList.appendChild(listElem);
- // Append the list item to the current list
-} else if (headLevel > prevLevel) {
-      // Start a new nested list
-      var nestedList = document.createElement("ol");
-      nestedList.appendChild(listElem);
-      // Append nested list to last item in the current list
-      outlineList.lastChild.appendChild(nestedList);
-      // Change the current list to the nested list
-      outlineList = nestedList;
-} else {
-      // Append the list item to a higher list
-      // Calculate the difference between the current and previous level
-}
-// Update the value of prevLevel
-prevLevel = headLevel;
+            var headLevel = headings.indexOf(n.nodeName);
+            if (headLevel !== -1) {
+                  //add an id to the heading if it is missing
+                  headNum++;
+                  if (n.hasAttribute("id") === false) {
+                        n.setAttribute("id", "head" + headNum);
+                  }
+                  var listElem = document.createElement("li");
+                  //create hypertext link to the document headings
+                  var linkElem = document.createElement("a");
+                  linkElem.innerHTML = n.innerHTML;
+                  linkElem.setAttribute("href", "#" + n.id);
 
-// Calculate the difference between the current and previous level
+                  //append the hypertext link to the list item
+                  listElem.appendChild(linkElem);
+
+                  if (headLevel === prevLevel) {
+                        //append the list item to the current list
+                        outlineList.appendChild(listElem);
+                  } else if (headLevel > prevLevel) {
+                        //start a new nested list
+                        var nestedList = document.createElement("ol")
+                        nestedList.appendChild(listElem);
+                        //append nested list to the last item in the current list
+                        outlineList.lastChild.appendChild(nestedList);
+                        //change the current list to the nested list
+                        outlineList = nestedList
+                  } else {
+                        //append the list item to a higher list
+                  }
+
+                  //update the value of prevLevel
+                  prevLevel = headLevel;
+            }
       }
-}
 }
